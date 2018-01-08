a53f4e0748315a765df0477fbe3e636c697c9fee
Nick Fox-Gieg <nick@fox-gieg.com>
2018-01-08 11:17:13 -0500

    cleanup

---
+++ Assets/ThirdParty/ImageEffects/Scripts/CRT_Effect.cs
@@ -0,0 +1,28 @@
+﻿// https://github.com/demonixis/Unity-toolbox
+
+using UnityEngine;
+using UnityStandardAssets.ImageEffects;
+
+[ExecuteInEditMode]
+public class CRT_Effect : PostEffectsBase {
+
+    public float Distortion = 0.1f;
+    public float InputGamma = 2.4f;
+    public float OutputGamma = 2.2f;
+    public float TextureSize = 768f;
+
+    private Material mat;
+
+    private void OnRenderImage(RenderTexture source, RenderTexture dest) {
+        if (mat == null) {
+            mat = new Material(Shader.Find("Custom/CRT"));
+        } else { 
+            mat.SetFloat("_Distortion", Distortion);
+            mat.SetFloat("_InputGamma", InputGamma);
+            mat.SetFloat("_OutputGamma", OutputGamma);
+            mat.SetVector("_TextureSize", new Vector2(TextureSize, TextureSize));
+            Graphics.Blit(source, dest, mat);
+        }
+    }
+
+}
