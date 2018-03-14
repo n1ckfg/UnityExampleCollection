41b14e0e9501184f1ec5daba28d6578e6eb64e29
n1ckfg <nick@fox-gieg.com>
2018-03-13 21:32:48 -0400

    update 2017.3

--- Assets/LipSync/Scripts/LipSyncMain.cs
+++ Assets/LipSync/Scripts/LipSyncMain.cs
@@ -24,16 +24,16 @@ public class LipSyncMain : MonoBehaviour {
 	public void Start() {
 		SetSmoothing(smoothing);
 		if (useMicrophone){
-			audio.playOnAwake = false;
-			audio.loop = true;
+			GetComponent<AudioSource>().playOnAwake = false;
+			GetComponent<AudioSource>().loop = true;
 			if (delay < 1) Debug.LogError ("Microphone Delay must be at least 1");
 			//audio.clip = Microphone.Start("", true, delay, micSampleRate);
 			if (Microphone.devices.Length > 0 && Microphone.devices.Length > micNumber){
-				audio.clip = Microphone.Start(Microphone.devices[micNumber], true, delay, micSampleRate);
+				GetComponent<AudioSource>().clip = Microphone.Start(Microphone.devices[micNumber], true, delay, micSampleRate);
 				//*** this is necessary or you get extreme latency problems ***
-				while(Microphone.GetPosition(audio.name) <= 0);
+				while(Microphone.GetPosition(GetComponent<AudioSource>().name) <= 0);
 			}
-			audio.Play();
+			GetComponent<AudioSource>().Play();
 		}
 	}
 
@@ -61,12 +61,12 @@ public class LipSyncMain : MonoBehaviour {
 
 	public void Update() {
 		//if (!audio.isPlaying){
-		if (audio == null) {
+		if (GetComponent<AudioSource>() == null) {
 			volume = 0.0f;
 		} else {
 			float min = 10000000.0f;
 			float max = -10000000.0f;
-			audio.GetOutputData (samples, 0);
+			GetComponent<AudioSource>().GetOutputData (samples, 0);
 			
 			float average = 0.0f;
 			for (int i = 0; i < winWidth; i++){
